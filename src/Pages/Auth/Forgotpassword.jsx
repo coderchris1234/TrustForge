74c2359045a6b344ac41d00f89e64883d40afb10
DevEvans-tech <uchechukwuevans3@gmail.com>
2025-11-17 14:41:07 +0100

    D

--- src/Pages/Auth/Forgotpassword.jsx
+++ src/Pages/Auth/Forgotpassword.jsx
@@ -26,7 +26,6 @@ const ForgotPassword = () => {
   const user = useSelector((state) => state.TrustForge.user);
   const BaseUrl = import.meta.env.VITE_BaseUrl;
 
-  // Dynamic endpoint selection based on role
   const endpoints =
     user?.data?.role === "Investor"
       ? `${BaseUrl}/forgoti`
